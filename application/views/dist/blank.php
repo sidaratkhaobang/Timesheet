39e7d161d4a6c4873fb21f161a14cdd7011b3960
Wichuta18 <kimweir9@gmail.com>
2020-02-03 17:33:17 +0700

    first commit

--- application/views/dist/blank.php
+++ application/views/dist/blank.php
@@ -6,7 +6,7 @@ $this->load->view('dist/_partials/header');
        <div class="main-content">
         <section class="section">
           <div class="section-header">
-            <h1>Table</h1>
+            <h1>Project</h1>
             <div class="section-header-breadcrumb">
               <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
               <div class="breadcrumb-item"><a href="#">Components</a></div>
@@ -17,7 +17,7 @@ $this->load->view('dist/_partials/header');
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>Advanced Table</h4>
+                    <h4>Project Table</h4>
                     <div class="card-header-form">
                       <form>
                         <div class="input-group">
