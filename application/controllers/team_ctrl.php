5612d84cd707de18651a80db8240b89194a5ed94
unknown <slotsnow0301@gmail.com>
2020-02-25 10:54:39 +0700

    team ctrl :pig:

---
+++ application/controllers/team_ctrl.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Team_ctrl extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Team_model');
+    }
+
+    public function team()
+    {
+        $data = array(
+            'title' => "Create Team"
+        );
+        $data['user'] = $this->Team_model->getUser();
+        $this->load->view('dist/admin-team_create', $data);
+    }
+
+    public function insert_data()
+    {
+        $data = array(
+            "nameteam" => $this->input->post("nameteam"),
+            "member" => implode(",",$this->input->post("member"))
+        );
+        $this->Team_model->insert($data);
+        $this->session->set_flashdata('add_success', TRUE);
+        redirect('team_ctrl/team', 'refresh');
+    }
+}
