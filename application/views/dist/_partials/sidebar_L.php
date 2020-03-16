12bcdae562c9f8f341620c65bef0fa8f2f926ef3
unknown <slotsnow0301@gmail.com>
2020-03-16 14:49:14 +0700

    edit name menu :pig:

--- application/views/dist/_partials/sidebar_L.php
+++ application/views/dist/_partials/sidebar_L.php
@@ -5,11 +5,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
             <a href="<?php echo base_url(); ?>leader_ctrl/status">Timesheet</a>
-        </div> 
+        </div>
         <ul class="sidebar-menu">
-            <li class="<?php echo $this->uri->segment(2) == 'status' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>Leader_ctrl/status"><i class="far fa-file-alt"></i> <span>Projects</span></a></li>
-            <li class="<?php echo $this->uri->segment(2) == 'components_wizard' ? 'active' : ''; ?>"><a class="nav-link beep beep-sidebar" href="<?php echo base_url(); ?>dist/components_wizard">Wizard</a></li>
             <li class="<?php echo $this->uri->segment(2) == 'dashboard' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>Leader_ctrl/dashboard"><i class="fas fa-fire"></i><span>Dashboard Project</span></a></li>
+            <li class="<?php echo $this->uri->segment(2) == 'status' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>Leader_ctrl/status"><i class="far fa-file-alt"></i> <span>Approve Projects</span></a></li>
             <li class="<?php echo $this->uri->segment(2) == 'assign' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>Leader_ctrl/assign"><i class="fas fa-user-friends"></i></i><span>Assignment of Member</span></a></li>
         </ul>
         <div class="mt-2 mb-18 p-3 hide-sidebar-mini">
