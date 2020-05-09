4f811417bd7882796f466a11a73f406b17ebd596
Wichuta18 <kimweir9@gmail.com>
2020-05-09 21:38:37 +0700

    close fucking script :alien:

--- assets/js/page/index-0.js
+++ assets/js/page/index-0.js
@@ -1,45 +1,45 @@
 "use strict";
 
-var statistics_chart = document.getElementById("myChart").getContext('2d');
+// var statistics_chart = document.getElementById("myChart").getContext('2d');
 
-var myChart = new Chart(statistics_chart, {
-  type: 'line',
-  data: {
-    labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    datasets: [{
-      label: 'Statistics',
-      data: [640, 387, 530, 302, 430, 270, 488],
-      borderWidth: 5,
-      borderColor: '#6777ef',
-      backgroundColor: 'transparent',
-      pointBackgroundColor: '#fff',
-      pointBorderColor: '#6777ef',
-      pointRadius: 4
-    }]
-  },
-  options: {
-    legend: {
-      display: false
-    },
-    scales: {
-      yAxes: [{
-        gridLines: {
-          display: false,
-          drawBorder: false,
-        },
-        ticks: {
-          stepSize: 150
-        }
-      }],
-      xAxes: [{
-        gridLines: {
-          color: '#fbfbfb',
-          lineWidth: 2
-        }
-      }]
-    },
-  }
-});
+// var myChart = new Chart(statistics_chart, {
+//   type: 'line',
+//   data: {
+//     labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+//     datasets: [{
+//       label: 'Statistics',
+//       data: [640, 387, 530, 302, 430, 270, 488],
+//       borderWidth: 5,
+//       borderColor: '#6777ef',
+//       backgroundColor: 'transparent',
+//       pointBackgroundColor: '#fff',
+//       pointBorderColor: '#6777ef',
+//       pointRadius: 4
+//     }]
+//   },
+//   options: {
+//     legend: {
+//       display: false
+//     },
+//     scales: {
+//       yAxes: [{
+//         gridLines: {
+//           display: false,
+//           drawBorder: false,
+//         },
+//         ticks: {
+//           stepSize: 150
+//         }
+//       }],
+//       xAxes: [{
+//         gridLines: {
+//           color: '#fbfbfb',
+//           lineWidth: 2
+//         }
+//       }]
+//     },
+//   }
+// });
 
 $('#visitorMap').vectorMap(
 {
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -629,6 +629,56 @@ $(function() {
   }
 });
 
+// //chart_emp
+// var ctx = document.getElementById("myChart2").getContext('2d');
+// var myChart = new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+//     datasets: [{
+//       label: 'Statistics',
+//       data: [460, 458, 330, 502, 430, 610, 488],
+//       borderWidth: 2,
+//       backgroundColor: 'rgba(254,86,83,.7)',
+//       borderColor: 'rgba(254,86,83,.7)',
+//       borderWidth: 2.5,
+//       pointBackgroundColor: '#ffffff',
+//       pointRadius: 4
+//     },{
+//       label: 'Statistics',
+//       data: [550, 558, 390, 562, 490, 670, 538],
+//       borderWidth: 2,
+//       backgroundColor: 'rgba(63,82,227,.8)',
+//       borderColor: 'transparent',
+//       borderWidth: 0,
+//       pointBackgroundColor: '#999',
+//       pointRadius: 4
+//     }]
+//   },
+//   options: {
+//     legend: {
+//       display: false
+//     },
+//     scales: {
+//       yAxes: [{
+//         gridLines: {
+//           drawBorder: false,
+//           color: '#f2f2f2',
+//         },
+//         ticks: {
+//           beginAtZero: true,
+//           stepSize: 150
+//         }
+//       }],
+//       xAxes: [{
+//         gridLines: {
+//           display: false
+//         }
+//       }]
+//     },
+//   }
+// });
+
 // step by step worker
 // $(document).ready(function() {
 //   var current = 1,
