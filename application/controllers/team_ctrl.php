757eaf37781b89865a5a5470d2b41cc6c642de78
unknown <slotsnow0301@gmail.com>
2020-05-24 14:50:59 +0700

    commit team ctrl

--- application/controllers/team_ctrl.php
+++ application/controllers/team_ctrl.php
@@ -12,7 +12,7 @@ class Team_ctrl extends CI_Controller
         $this->load->model('Team_model');
     }
 
-    public function team()
+    public function form_team()
     {
         $data = array(
             'title' => "Create Team"
@@ -23,12 +23,15 @@ class Team_ctrl extends CI_Controller
 
     public function insert_data()
     {
-        // $query = $this->db->get('teams');
-        // $num = $query->num_rows();
-        // if ($num > 0) {
-        //     $this->session->set_flashdata('data_duplicate', TRUE);
-        //     redirect('team_ctrl/team', 'refresh');
-        // } else {
+        $team_name = $this->input->post("team_name");
+        $this->db->select('team_name');
+        $this->db->where('team_name', $team_name);
+        $query = $this->db->get('teams');
+        $num = $query->num_rows();
+        if ($num > 0) {
+            $this->session->set_flashdata('data_duplicate', TRUE);
+            redirect('team_ctrl/dataTeam', 'refresh');
+        } else {
         $data = array(
             "team_name" => $this->input->post("team_name"),
             "member" => implode(",", $this->input->post("member"))
@@ -36,7 +39,7 @@ class Team_ctrl extends CI_Controller
         $this->Team_model->insert($data);
         $this->session->set_flashdata('add_success', TRUE);
         redirect('team_ctrl/dataTeam', 'refresh');
-        // }
+        }
     }
 
     public function dataTeam()
@@ -46,5 +49,29 @@ class Team_ctrl extends CI_Controller
         );
         $data['data_team'] = $this->Team_model->getdata();
         $this->load->view('dist/admin-team-view', $data);
+    }
+
+    public function update_team($id)
+    {
+        $data = array(
+            'title' => "Update Team"
+        );
+        $data['user'] = $this->Team_model->getUser();
+        $data['data'] = $this->Team_model->getbyID($id);
+        $this->load->view('dist/admin-team-update', $data);
+    }
+
+    public function update_data($id)
+    {
+        $this->Team_model->update($id);
+        $this->session->set_flashdata('save_update', TRUE);
+        redirect('team_ctrl/dataTeam', 'refresh');
+    }
+
+    function delete()
+    {
+        $id = $this->input->post('delete_id', TRUE);
+        $this->Team_model->delete_package($id);
+        redirect('team_ctrl/dataTeam');
     }
 }
